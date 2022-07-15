654169b6ddab76282e84ee6696a042bb25251510
ImageBlue <75412866+ImageBlue@users.noreply.github.com>
2022-07-15 16:50:09 +0800

    Update mainwindow.cpp
    添加了通过LineEdit和PushButton输入PID和V参数的功能

--- OController/mainwindow.cpp
+++ OController/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <iostream>
 #include <QtSerialPort/QSerialPort>
 #include <QtSerialPort/QSerialPortInfo>
 #include <QTimer>
@@ -8,18 +9,33 @@
 #include <QBitArray>
 #include <QMessageBox>
 #include <QByteArray>
+#include <QString>
 
-bool isManu = true;
+
+bool isManu = true;                  //手动模式开关，默认手动模式
+float fParaP = 1.0;                  //PID参数
+float fParaI = 1.0;
+float fParaD = 1.0;
+int iParaV = 0;                      //初始流速
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+    /******************************************初始化控件*********************************************/
     ui->setupUi(this);
     this->setFixedSize(750,500);
 
     ui->tabWidget->setFixedSize(180,460);
 
     ui->ManuRButton->setChecked(true);
+
+    ui->PlineEdit->setText("1.0");
+    ui->IlineEdit->setText("1.0");
+    ui->DlineEdit->setText("1.0");
+    ui->VlineEdit->setText("0");
+
+    /******************************************初始化槽函数********************************************/
     connect(ui->ManuRButton, &QRadioButton::clicked, [=](){
         isManu = true;
         ui->SetNewButton->setEnabled(isManu);
@@ -44,3 +60,55 @@ void MainWindow::on_CloseButton_clicked()
     exit(0);
 }
 
+
+void MainWindow::on_SetNewButton_clicked()
+{
+    QString qstrP, qstrI, qstrD;          //读取LineEdit中的字符串
+    qstrP.clear();
+    qstrI.clear();
+    qstrD.clear();
+
+    qstrP = ui->PlineEdit->text();
+    qstrI = ui->IlineEdit->text();
+    qstrD = ui->DlineEdit->text();
+
+    float fTempP, fTempI, fTempD;         //字符串转float（暂存，等待合法性判断）
+    fTempP = qstrP.toFloat();
+    fTempI = qstrI.toFloat();
+    fTempD = qstrD.toFloat();
+
+    if(fTempP > 0.0 && fTempI > 0.0 && fTempD > 0.0)  //数据合法，更新参数
+    {
+        fParaP = fTempP;
+        fParaI = fTempI;
+        fParaD = fTempD;
+        qDebug() << "P:" << fParaP << " I:" << fParaI << " D:" << fParaD;
+        //TODO:串口发送参数
+    }
+    else
+    {
+        qDebug() << "Invalid Parameter(s)!";
+    }
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString qstrV;                       //读取LineEdit中的字符串
+    qstrV.clear();
+
+    qstrV = ui->VlineEdit->text();       //字符串转int（暂存，等待合法性判断）
+
+    int iTempV = qstrV.toInt();
+
+    if(iTempV >= 0)                       //数据合法，更新参数
+    {
+        iParaV = iTempV;
+        qDebug() << "Velocity:" << iParaV;
+        //TODO:串口发送参数
+    }
+    else
+    {
+        qDebug() << "Invalid Parameter!";
+    }
+}
