56a1b95441ad0c631a4586a50de28cb83a0708c7
ImageBlue <75412866+ImageBlue@users.noreply.github.com>
2022-07-21 22:08:50 +0800

    Add files via upload

---
+++ OController/mainwindow.h
@@ -0,0 +1,54 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+#include <QChartView>
+#include <QtCharts>
+#include <QString>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+
+    void on_CloseButton_clicked();
+
+    void on_SetNewButton_clicked();
+
+    void on_VNewButton_clicked();
+
+    void on_ConnectSerialButton_clicked();
+
+    void on_SearchSeiralButton_clicked();
+
+    void on_CloseSerialButton_clicked();
+
+    void sleep(int msec);       //延时函数
+
+    void on_ClearButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    QSerialPort* SerialPort;
+
+    QChart *chart;
+    QChartView *chartView;
+    QValueAxis *AxisXT;        //时间坐标
+    QValueAxis *AxisYV;        //实时流速坐标
+    QSplineSeries *sSeriesV;
+
+    QDateTime curTime;
+    QString strTime;
+};
+#endif // MAINWINDOW_H
