8e3573950b28e542578b53862c863ab1bed731e0
ImageBlue <75412866+ImageBlue@users.noreply.github.com>
2023-02-28 12:04:59 +0800

    Add MQTT functions

---
+++ Ocontroller/Ocontroller.pro
@@ -0,0 +1,36 @@
+QT       += core gui
+QT       += serialport network
+QT       += core gui charts
+QT       += mqtt
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+CONFIG += c++11
+
+# You can make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+SOURCES += \
+    AsyncMQTT.cpp \
+    AsyncSerialPort.cpp \
+    main.cpp \
+    mainwindow.cpp
+
+HEADERS += \
+    AsyncMQTT.h \
+    AsyncSerialPort.h \
+    mainwindow.h
+
+FORMS += \
+    mainwindow.ui
+
+TRANSLATIONS += \
+    Ocontroller_zh_CN.ts
+CONFIG += lrelease
+CONFIG += embed_translations
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /opt/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
---
+++ Ocontroller/mainwindow.h
@@ -0,0 +1,90 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtSerialPort/QSerialPort>
+#include <QtSerialPort/QSerialPortInfo>
+#include <QChartView>
+#include <QtCharts>
+#include <QString>
+#include <iostream>
+#include <QTimer>
+#include <QTime>
+#include <QDebug>
+#include <QBitArray>
+#include <QMessageBox>
+#include <QByteArray>
+#include <QThread>
+#include "AsyncSerialPort.h"
+#include "AsyncMQTT.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+
+    void on_CloseButton_clicked();
+
+    void on_SetNewButton_clicked();
+
+    void on_VNewButton_clicked();
+
+    void on_ConnectSerialButton_clicked();
+
+    void on_SearchSeiralButton_clicked();
+
+    void on_CloseSerialButton_clicked();
+
+    void sleep(int msec);       //延时函数
+
+    void SerialPort_Receive(QByteArray buffer);
+
+    void on_ClearButton_clicked();
+
+    void Paint_Data();
+
+    void on_WLConnectButton_clicked();
+
+    void on_WLDisconnectButton_clicked();
+
+    void MQTT_Receive(QByteArray buffer);
+
+    void on_ManuRButton_clicked();
+
+    void on_AutoRButton_clicked();
+
+private:
+    Ui::MainWindow *ui;
+    //QSerialPort* SerialPort;
+
+    QTimer* ConnectSerialTimer;
+    QTimer* writetimer;
+    QTimer* painttimer;
+
+    QChart *chart;
+    QChartView *chartView;
+    QValueAxis *AxisXT;        //时间坐标
+    QValueAxis *AxisYV;        //实时流速坐标
+    QSplineSeries *sSeriesV;
+
+    //QDateTime curTime;
+    QString strTime;
+
+    AsyncSerialPort *ASerialPort;
+
+    AsyncMQTT *AMqtt;
+
+signals:
+    void Send_to_SerialPort(QString qstrS);
+    void Send_to_MQTT(QString qstrS);
+};
+#endif // MAINWINDOW_H
